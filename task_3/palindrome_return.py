0c0a1be4562be66e5994280194c2561a9782c071
a-dugonin <a_dugonin@mail.ru>
2024-04-25 11:21:10 +0300

    task_3: Добавление аннотаций типов

--- task_3/palindrome_return.py
+++ task_3/palindrome_return.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 
-def can_be_poly(string: str):
+def can_be_poly(string: str) -> bool or str:
     """Функция проверяет, является ли переданная на вход строка палиндромом"""
     if isinstance(string, str):
         return len(string) % 2 == sum(value % 2 for value in Counter(string).values())
--- tests/test_palindrome_return.py
+++ tests/test_palindrome_return.py
@@ -4,7 +4,7 @@ from task_3.palindrome_return import can_be_poly
 
 
 class TestCanBePoly(unittest.TestCase):
-    def test_can_be_poly(self):
+    def test_can_be_poly(self) -> None:
         self.assertEqual(can_be_poly('abcba'), True)
         self.assertEqual(can_be_poly('abbbc'), False)
         self.assertEqual(can_be_poly(5), "В функцию передан не строковый аргумент")
